edb397e1842177b72e4a605ff348cb99ce5fc1a7
Tochi Ezeamama <judithezeamama@gmail.com>
2024-07-02 09:30:47 +0100

    Port

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/api/hello', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
